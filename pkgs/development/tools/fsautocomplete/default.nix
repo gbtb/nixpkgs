496899516196652fdde7d56a8c9c118384d3771b
gbtb <goodbetterthebeast3@gmail.com>
2022-12-16 00:20:01 +1000

    fixed nuget source paths leaking into closure

--- pkgs/development/tools/fsautocomplete/default.nix
+++ pkgs/development/tools/fsautocomplete/default.nix
@@ -9,11 +9,10 @@ let
       nugetDeps = { fetchNuGet }: [ (fetchNuGet { inherit pname version; sha256 = "sha256-xZFTxdD4ma5IlPZNqOSwJv/1ixicm21FY+WVS574QiI="; }) ];
     };
     dontUnpack = true;
-    dontConfigure = true;
+    #dontConfigure = true;
     dontInstall = true;
     isDotnetTool = true;
-    executables = ".dotnet/tools/${pname}";
-    buildPhase = ''
+    configurePhase = ''
          # Generate a NuGet.config file to make sure everything,
          # including things like <Sdk /> dependencies, is restored from the proper source
       cat <<EOF > "./NuGet.config"
@@ -25,11 +24,19 @@ let
         </packageSources>
       </configuration>
       EOF
+    '';
+
+    executables = ".dotnet/tools/${pname}";
+    buildPhase = ''
           #set env variable to install tool where fixupHook will be able to find it
           export DOTNET_CLI_HOME=$out/lib/${pname}
           dotnet tool install --configfile ./NuGet.config --global fsautocomplete 
+
+          cd $out
+          #removing text files that contains nix store paths to temp nuget sources we made
+          find . -name 'project.assets.json' -delete
+          find . -name '.nupkg.metadata' -delete
     '';
-    nuget-source = null;
   };
 in
 fsautocomplete
